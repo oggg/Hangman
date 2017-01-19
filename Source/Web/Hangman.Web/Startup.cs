33a06a2cdcd004366197d77978a2170b9761ba9a
oggg <ogggster@gmail.com>
2017-01-19 22:43:20 +0200

    initial project structure

---
+++ Source/Web/Hangman.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hangman.Web.Startup))]
+namespace Hangman.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
