62edf24b331a9511fc36944f9246bb851c2853ea
oggg <ogggster@gmail.com>
2017-01-19 23:53:50 +0200

    Removing unnecessary fields in User table

---
+++ Source/Data/Hangman.Data/HangmanDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Entity;
+using Hangman.Web.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Hangman.Data
+{
+    public class HangmanDbContext : IdentityDbContext<User>
+    {
+        public HangmanDbContext()
+            : base("HangmanConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static HangmanDbContext Create()
+        {
+            return new HangmanDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().Ignore(u => u.AccessFailedCount)
+                                       .Ignore(u => u.EmailConfirmed)
+                                       .Ignore(u => u.LockoutEnabled)
+                                       .Ignore(u => u.LockoutEndDateUtc)
+                                       .Ignore(u => u.PhoneNumber)
+                                       .Ignore(u => u.PhoneNumberConfirmed)
+                                       .Ignore(u => u.TwoFactorEnabled);
+        }
+    }
+}
